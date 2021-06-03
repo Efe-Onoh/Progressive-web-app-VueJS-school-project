1f6aa6036489b1bbef96672cfd8c13b4c0dfc746
Efe O <ednaellenpope@gmail.com>
2021-06-03 14:22:24 +0400

    new files added

---
+++ product.js
@@ -0,0 +1,48 @@
+let products = [
+
+        {
+            id: 1001,
+            title: "cat food, 25lb bag",
+            description: "a 25 pound bag of <em>irresistible</em>," + "organic goodness for your cat.",
+            price: 2000,
+            image: "images/download.jpg",
+            availableInventory: 10,
+            rating: 3
+        },
+        {
+            id: 1002,
+            title: "yarn",
+            description: "Yarn a 25 pound bag of <em>irresistible</em>," + "organic goodness for your cat.",
+            price: 200,
+            image: "images/download.jpg",
+            availableInventory: 10,
+            rating: 3
+        },
+        {
+            id: 1003,
+            title: "cat nip",
+            description: "Cat nip a 25 pound bag of <em>irresistible</em>," + "organic goodness for your cat.",
+            price: 1000,
+            image: "images/download.jpg",
+            availableInventory: 10,
+            rating: 4
+        },
+        {
+            id: 1004,
+            title: "wheat",
+            description: " Wheat a 25 pound bag of <em>irresistible</em>," + "organic goodness for your cat.",
+            price: 2400,
+            image: "images/download.jpg",
+            availableInventory: 10,
+            rating: 1
+        },
+        {
+            id: 1005,
+            title: "milk",
+            description: "cat milk a 25 pound bag of <em>irresistible</em>," + "organic goodness for your cat.",
+            price: 1500,
+            image: "images/download.jpg",
+            availableInventory: 10,
+            rating: 2
+        }
+]
